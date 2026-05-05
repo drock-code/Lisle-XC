0853a5e5adbf683d13bee4cd0657bd561a934dd2
Derek Miller <dmiller1984@gmail.com>
2026-05-04 19:11:10 -0500

    Athlete Handbook
    Added handbooks from 2025 and 2026.

--- lisle-xc/components/QuickLinks.tsx
+++ lisle-xc/components/QuickLinks.tsx
@@ -6,8 +6,8 @@ export default function QuickLinks() {
       </h3>
       <ul className="space-y-4">
         <li className="flex items-center space-x-3 group cursor-pointer">
-          <div className="w-2 h-2 bg-light-blue rounded-full group-hover:scale-150 transition-transform"></div>
-          <span className="font-body font-medium hover:text-light-blue transition-colors">Athlete Handbook</span>
+          <div className="w-2 h-2 bg-light-blue rounded-full group-hover:scale-150 transition-transform shrink-0"></div>
+          <span className="font-body font-medium hover:text-light-blue transition-colors"><Link href="/files/athleteHandbook2026.pdf">Athlete Handbook</Link></span>
         </li>
         <li className="flex items-center space-x-3 group cursor-pointer">
           <div className="w-2 h-2 bg-light-blue rounded-full group-hover:scale-150 transition-transform"></div>
